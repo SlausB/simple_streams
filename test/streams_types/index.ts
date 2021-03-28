c0bf3ed3b8b5907f8ee73978812c8d65b03876d8
Slaus <SlavMFM@gmail.com>
2021-03-28 23:50:11 +0400

    fixing previous

--- test/streams_types/index.ts
+++ test/streams_types/index.ts
@@ -1,17 +1,21 @@
 import child_process from 'child_process'
 import { expect } from 'chai'
 import assert from 'assert'
-import fs, { unlinkSync } from 'fs'
+import fs, { unlinkSync, existsSync } from 'fs'
+
+const STREAMS_TYPES = 'streams_types.json'
 
 function compare( source : string, expectation : string ) {
     //try { child_process.execSync( 'rm streams_types.json' ) } catch(e:any){}
-    try { unlinkSync( 'streams_types.json' ) } catch(e:any){}
+    //can't achieve test passing on GitLab CI when file doesn't exist:
+    if ( existsSync( STREAMS_TYPES ) )
+        unlinkSync( STREAMS_TYPES )
 
     //invoking the compiler:
     child_process.execSync( 'npx ttsc --noEmit ' + source )
     
     assert.deepStrictEqual(
-        JSON.parse( fs.readFileSync( 'streams_types.json', 'utf8' ) ),
+        JSON.parse( fs.readFileSync( STREAMS_TYPES, 'utf8' ) ),
         JSON.parse( fs.readFileSync( expectation, 'utf8' ) ),
     )
 }
